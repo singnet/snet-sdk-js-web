9d9ab88ec0f98f1328a1b64f964716b52dcaefee
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-12-05 17:12:06 +0300

    [SPT-728] updated example for calling service with preselected strategy

--- examples/calculator/src/App.jsx
+++ examples/calculator/src/App.jsx
@@ -1,36 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css"
-import { getServiceClient } from "./helperFunctions/sdkCallFunctions";
+import { generateOptions, getDefaultServiceClient, getFreeCallServiceClient, getPaymentServiceClient, getServiceMetadata } from "./helperFunctions/sdkCallFunctions";
 import ServiceDemo from "./components/ServiceDemo";
 import ServiceInfo from "./components/ServiceInfo";
 import WalletInfo from "./components/WalletInfo";
+import { isEmpty } from "lodash";
 
 const ExampleService = () => {
+  const [serviceMetadata, setServiceMetadata] = useState();
   const [serviceClient, setServiceClient] = useState();
   const [isClientLoading, setIsClientLoading] = useState(false);
+  // const [options, setOptions] = useState(generateOptions());
+  const options = generateOptions();
+  
+  const getServiceClientHandler = async (getServiceClient) => {
+    if (isClientLoading) {
+      return;
+    }
+    setIsClientLoading(true);
+    const serviceClient = await getServiceClient(serviceMetadata);
+    setServiceClient(serviceClient);
+    setIsClientLoading(false);
+  }
 
-  // useEffect(() => {
-  //   console.log("ExampleService init");
-    
-    const getServiceData = async () => {
-      if (isClientLoading) {
+  const serviceClientButtons = [
+      {
+        key: "default",
+        title: "Load service client",
+        clientLoader: getDefaultServiceClient
+      },
+      {
+        key: "paid",
+        title: "Load Paid Strategy client",
+        clientLoader: getPaymentServiceClient
+      },
+      {
+        key: "freeCall",
+        title: "Load Free-call client",
+        clientLoader: getFreeCallServiceClient
+      }
+  ]
+
+  useEffect(() => {
+    const getServiceMetadataFromSDK =  async () => {
+      if (!isEmpty(serviceMetadata)) {
         return;
       }
-      setIsClientLoading(true);
-      const serviceClient = await getServiceClient();
-      setServiceClient(serviceClient);
-      setIsClientLoading(false);
+      const metadata =  await getServiceMetadata(options);
+      setServiceMetadata(metadata);
     }
 
-  //   getServiceData()
-  // }, [])
+     getServiceMetadataFromSDK();
+  }, [options])
 
     return (
       <div className="service-container">
-        <button onClick={getServiceData}>load service client</button>
         <div className="service-info">
-          <ServiceInfo serviceClient={serviceClient} />
-          <WalletInfo />
+          <ServiceInfo serviceMetadata={serviceMetadata} />
+          <WalletInfo serviceMetadata={serviceMetadata}/>
+        </div>
+        <div className="loadServiceButtons">
+          {serviceClientButtons.map(button => <button key={button.key} disabled={!serviceMetadata} onClick={() => getServiceClientHandler(button.clientLoader)}>{button.title}</button>
+          )}
         </div>
         <ServiceDemo serviceClient={serviceClient}/>
       </div>
