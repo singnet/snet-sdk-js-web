40778307d32c46501d05319f6ad84515c68b9b26
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-12-06 15:52:26 +0300

    [SPT-727] create free-calls config file for calculator example

---
+++ examples/calculator/src/configs/freecallsConfig.js
@@ -0,0 +1,8 @@
+export default {
+    disableBlockchainOperations: false,
+    tokenExpirationBlock: 7389838,
+    tokenToMakeFreeCall:
+        'af33f4064de904183ffe5447dd44554c0123815fda503c9f8a50b26b291cf16a78de705c6d85077ddc673f55333f9383382c5330c4f803278940c52df03788631b',
+    email: 'fospipakno@gufum.com',
+    concurrency: true,
+};
