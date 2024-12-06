c59fb4f64167c23719b73b22671a58adf96e7fb4
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-12-06 15:51:11 +0300

    [SPT-728] moved get free-calls button to service info component

--- examples/calculator/src/components/WalletInfo/index.jsx
+++ examples/calculator/src/components/WalletInfo/index.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import { getWalletInfo, getAvailableFreeCalls } from "../../helperFunctions/sdkCallFunctions";
+import { getWalletInfo } from "../../helperFunctions/sdkCallFunctions";
 import { cogsToAgix, tokenName } from "../../helperFunctions/priceHelpers";
 import "./styles.css";
 import Loader from "../Loader/index.jsx";
+import Table from "../Table/index.jsx";
 
 const WalletInfo = ({serviceMetadata}) => {
     const [address, setAddress] = useState('');
     const [balance, setBalance] = useState('');
     const [transactionCount, setTransactionCount] = useState('');
     const [isLoading, setIsLoading] = useState(false);
-    const [availableFreeCalls, setAvailibleFreeCalls] = useState("");
 
     const getWalletInfoFromSDK = async () => {
         setIsLoading(true);
@@ -26,28 +26,13 @@ const WalletInfo = ({serviceMetadata}) => {
         { title: "transactions", value: transactionCount },
     ]
 
-    const getAvailableFreeCallsFromSDK = async () => {
-        setAvailibleFreeCalls(await getAvailableFreeCalls(serviceMetadata));
-    }
-
     return (
         <div className="wallet-info">
-            <div className="wallet-info-button-group">
+            <div className="button-group">
                 <button onClick={getWalletInfoFromSDK}>Get wallet info</button>
-                <button disabled={!serviceMetadata} onClick={getAvailableFreeCallsFromSDK}>Get freecalls available</button>
             </div>
             <Loader isLoading={isLoading} />
-            {address && balance &&
-            <table className="wallet-info-table">
-                <tbody>
-                    {walletInfoMeta.map(infoRow =>
-                    <tr key={infoRow.title}>
-                        <th scope="row">{infoRow.title}</th>
-                        <td>{infoRow.value}</td>
-                    </tr>)}
-                </tbody>
-            </table>}
-            <p>Available free-calls: <b>{availableFreeCalls}</b></p>
+            {address && balance && <Table tableData={walletInfoMeta} />}
         </div>
 )}
 
