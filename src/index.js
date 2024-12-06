2c1ecf1f99ba867b2baa58cb775c59aa8222d29f
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-12-06 18:06:58 +0300

    [SPT-746] added TrainingProvider for import

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ export * from 'snet-sdk-core';
 export * from './payment_strategies';
 export { default as WebServiceClient } from './WebServiceClient';
 export { default as ServiceMetadataProviderWeb } from './ServiceMetadataProvider';
+export { default as TrainingProviderWeb } from './training/TrainingProvider';
 
 export default WebSdk;
