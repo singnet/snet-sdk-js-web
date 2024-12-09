a780a07196334742d6253af53f82b2a2c2e05f33
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-12-09 18:46:13 +0300

    [SPT-679] removed export of WebServiceClient, ServiceMetadataProviderWeb, TrainingProviderWeb

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import WebSdk from './WebSdk';
 
 export * from 'snet-sdk-core';
 export * from './payment_strategies';
-export { default as WebServiceClient } from './WebServiceClient';
-export { default as ServiceMetadataProviderWeb } from './ServiceMetadataProvider';
-export { default as TrainingProviderWeb } from './training/TrainingProvider';
 
 export default WebSdk;
